d929911df3de31bdf6628b88de82484a5ee20973
Kamil <kwolanski3@gmail.com>
2026-01-13 12:54:51 +0100

    cisterian numbers 60-99

--- src/cistercian/dictionary.ts
+++ src/cistercian/dictionary.ts
@@ -1,4 +1,14 @@
-import { base1, base2, base3, base4, base5 } from "./bases";
+import {
+  base1,
+  base2,
+  base3,
+  base4,
+  base5,
+  base6,
+  base7,
+  base8,
+  base9,
+} from "./bases";
 import { rotateY } from "./transforms";
 import type { LineDef } from "./types";
 
@@ -17,4 +27,16 @@ export const cistercianNumDictionary: Record<number, LineDef[]> = {
 
   5: base5,
   50: base5.map(rotateY),
+
+  6: base6,
+  60: base6.map(rotateY),
+
+  7: base7,
+  70: base7.map(rotateY),
+
+  8: base8,
+  80: base8.map(rotateY),
+
+  9: base9,
+  90: base9.map(rotateY),
 };
