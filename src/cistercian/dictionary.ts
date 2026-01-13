0c538e65b6d5c85c4c1171625255b59e3877528a
Kamil <kwolanski3@gmail.com>
2026-01-13 13:02:21 +0100

    rotateX function

--- src/cistercian/dictionary.ts
+++ src/cistercian/dictionary.ts
@@ -9,34 +9,43 @@ import {
   base8,
   base9,
 } from "./bases";
-import { rotateY } from "./transforms";
+import { rotateX, rotateY } from "./transforms";
 import type { LineDef } from "./types";
 
 export const cistercianNumDictionary: Record<number, LineDef[]> = {
   1: base1,
   10: base1.map(rotateY),
+  100: base1.map(rotateX),
 
   2: base2,
   20: base2.map(rotateY),
+  200: base2.map(rotateX),
 
   3: base3,
   30: base3.map(rotateY),
+  300: base3.map(rotateX),
 
   4: base4,
   40: base4.map(rotateY),
+  400: base4.map(rotateX),
 
   5: base5,
   50: base5.map(rotateY),
+  500: base5.map(rotateX),
 
   6: base6,
   60: base6.map(rotateY),
+  600: base6.map(rotateX),
 
   7: base7,
   70: base7.map(rotateY),
+  700: base7.map(rotateX),
 
   8: base8,
   80: base8.map(rotateY),
+  800: base8.map(rotateX),
 
   9: base9,
   90: base9.map(rotateY),
+  900: base9.map(rotateX),
 };
--- src/cistercian/transforms.ts
+++ src/cistercian/transforms.ts
@@ -6,3 +6,10 @@ export const rotateY = (line: LineDef): LineDef => ({
   x1: -line.x1,
   x2: -line.x2,
 });
+
+// (x,y)→(x,−y)
+export const rotateX = (line: LineDef): LineDef => ({
+  ...line,
+  y1: -line.y1,
+  y2: -line.y2,
+});
